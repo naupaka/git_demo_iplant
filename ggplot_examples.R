843d4456d13162bfcf9633f7f1b19a64d7a9a9df
Naupaka Zimmerman <naupaka@gmail.com>
2015-02-22 15:38:50 -0700

    Finish comment

--- ggplot_examples.R
+++ ggplot_examples.R
@@ -30,4 +30,5 @@ df  <- melt(iris, id.vars = "Species")
 ggplot(df, aes(Species, value, fill = variable)) +
     geom_bar(stat = "identity", position = "dodge") +
     scale_fill_brewer(palette = "Set1")
-￼
+
+# Done with lecture
