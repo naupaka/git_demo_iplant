ce741f9146290f1be45cbc88c8c89df45cd2d7f3
Naupaka Zimmerman <naupaka@gmail.com>
2015-02-22 14:45:15 -0700

    Add some additional R code, up till boxplot

--- ggplot_examples.R
+++ ggplot_examples.R
@@ -1,3 +1,26 @@
 # ggplot learning script
 # Naupaka Zimmerman naupaka@gmail.com
 # February 22, 2015
+
+install.packages("ggplot2", dependencies = TRUE)
+install.packages("plyr")
+install.packages("ggthemes")
+install.packages("reshape2")
+
+library("ggplot2")
+library("reshape2")
+library("ggthemes")
+library("plyr")
+
+myplot <- ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width)) + geom_point()
+myplot 
+
+d2 <- diamonds[sample(1:dim(diamonds)[1], 1000), ]
+
+dim(diamonds)
+dim(diamonds)[1]
+
+ggplot(d2, aes(carat, price, color=color)) + geom_point()
+
+library(MASS)
+ggplot(birthwt, aes(factor(race), bwt)) + geom_boxplot()
